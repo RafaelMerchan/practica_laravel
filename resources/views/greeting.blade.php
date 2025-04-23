6e7431f4cebd7d2514703ab65f4623cc5b3d1f0f
Rafael Merchan <rmerchan@svempresas.local>
2025-04-23 11:14:57 -0500

    chapter 1 done

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,8 @@
+<x-base>
+    <x-slot name="name">Rafael</x-slot>
+
+    <x-slot name="content">
+        <h1 class="text-4xl font-bold">Hello World</h1>
+        <p class="mt-4 text-lg">Welcome to the simple Blade component example.</p>
+    </x-slot>
+</x-base>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\GreetingController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::get('/home', function () {
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/hello', [GreetingController::class, 'index']);

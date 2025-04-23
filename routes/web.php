01609325f1717c6349179128d8c5c252a9878567
Rafael Merchan <rmerchan@svempresas.local>
2025-04-23 12:59:53 -0500

    chapter 2 hecho

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,26 @@
 
 use App\Http\Controllers\GreetingController;
 use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Models\Student;
 
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/', function (){
+    //$users = DB::select('select * from users2');
+    
+    /* $users = DB::table('users2')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
+            
+    dd($users); */
+
+    $student = Student::where('email','corrinboy@nohr.gob')->first();
+    //dd($students);
+
+
+    $student->status = 'active';
+    $student->save();
+    dd($student);
+
+});
 
 Route::get('/home', function () {
     return view('home');
